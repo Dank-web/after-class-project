1b10e8b0763ceace6937cb1135edcd8bf7eb766b
Dank-web <nusratjahanmoon27@gmail.com>
2026-01-24 00:37:20 +0600

    Add files via upload

---
+++ storing-birthdays.py
@@ -0,0 +1,19 @@
+f1n = "Hossain"
+f1b = "02 October"
+f2n = "Razvir"
+f2b = "03 January"
+f3n = "Ayaan"
+f3b = "25 January"
+f4n = "Muhib"
+f4b = "12 October"
+f5n = "Mehfam"
+f5b = "06 October"
+print("my 1st friend is",f1n, "and his birthday is on", f1b)
+print("my 2st friend is",f2n, "and his birthday is on", f2b)
+print("my 3st friend is",f3n, "and his birthday is on", f3b)
+print("my 4st friend is",f4n, "and his birthday is on", f4b)
+print("my 5st friend is",f5n, "and his birthday is on", f5b)
+name = input("enter your name:")
+print("\nHello", name)
+birthday = input("enter your birthday:")
+print( "Today is ", birthday,"so","Happy birthday", name)
